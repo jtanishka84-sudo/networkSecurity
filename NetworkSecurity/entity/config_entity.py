d8c3e314a507111a4ef914b044d50889f6b26ba4
jtanishka84-sudo <jtanishka84@gmail.com>
2026-09-19 17:36:07 +0530

    The first commit

--- NetworkSecurity/entity/config_entity.py
+++ NetworkSecurity/entity/config_entity.py
@@ -5,7 +5,7 @@ print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
 
 
-class TrainingpipelineConfig:
+class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
             timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
             self.pipeline_name=training_pipeline.PIPELINE_NAME
@@ -15,7 +15,7 @@ class TrainingpipelineConfig:
             self.timestamp: str=timestamp
 
 class DataIngestionConfig:
-    def __init__(self,training_pipeline_config:TrainingpipelineConfig):
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str=os.path.join(
               training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME 
         )
@@ -33,7 +33,7 @@ class DataIngestionConfig:
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
 
 class DataValidationConfig:
-    def __init__(self,training_pipeline_config:TrainingpipelineConfig):
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
         self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
         self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
@@ -46,3 +46,13 @@ class DataValidationConfig:
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
         )
+
+class DataTransformationConfig:
+     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.components.data_validation import DataValidation
+from NetworkSecurity.components.data_transformation import DataTransformation
+
 from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
-from NetworkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,TrainingpipelineConfig
+from NetworkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,TrainingPipelineConfig
 import sys
 if __name__=='__main__':
     try:
-        trainingpipelineconfig=TrainingpipelineConfig()
+        trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiated Completed")
         print(dataingestionartifact)
+
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data validation completed")
         print(data_validation_artifact)
+
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation completed")
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
